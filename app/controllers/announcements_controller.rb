1849445070a7ec400f9868680be49d3f885c6924
Tristan Gruener <tristan.gruener@gmail.com>
2016-12-21 10:21:52 -0800

    added image to announcement widget

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -70,6 +70,6 @@ class AnnouncementsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def announcement_params
-      params.require(:announcement).permit(:content)
+      params.require(:announcement).permit(:content, :image)
     end
 end
