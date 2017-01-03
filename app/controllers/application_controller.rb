38285a6fed9b5121406907c16dc18db70e1fae0b
Tristan Gruener <tristan.gruener@gmail.com>
2017-01-03 11:20:50 -0800

    converted header classes to h1 h2 h3 respecively

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,13 @@ class ApplicationController < ActionController::Base
 
   def this_week_widget
     @class_pages = ClassPage.order("order_position ASC").all
+    #how do i only pull the relevant dates?
+
+    #today = Date.today # Today's date
+    #@current_week = (today.at_beginning_of_week..today.at_end_of_week).map
+
+    #cycle through the first day and look for all events with that date
+    #repeat for each day of the week
   end
 
   def announcement_widget
