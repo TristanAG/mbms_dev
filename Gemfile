8a8f9fafd72b279f25ab4a1d6d457924cb3e526d
Tristan Gruener <tristan.gruener@gmail.com>
2017-01-31 23:50:15 -0800

    added s3 heroku settings

--- Gemfile
+++ Gemfile
@@ -2,6 +2,9 @@ source 'https://rubygems.org'
 
 ruby '2.3.1'
 
+
+gem 'aws-sdk', '< 2.0'
+
 gem "paperclip", '~> 5.0.0'
 gem "friendly_id"
 gem 'simple_calendar', '~> 2.0'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  
+
   # Settings specified here will take precedence over those in config/application.rb.
   Paperclip.options[:command_path] = "/usr/bin/"
 
@@ -54,4 +54,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  
+
 end
