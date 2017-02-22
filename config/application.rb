becbf62b28324f5c0d23e4686e590f55977f0add
Tristan Gruener <tristan.gruener@gmail.com>
2017-02-22 15:06:44 -0800

    changing timezone

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module MbmsDev
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.time_zone = 'Pacific Time (US & Canada)'
+    config.time_zone = 'America/Los_Angeles'
     config.beginning_of_week = :sunday
 
   end
