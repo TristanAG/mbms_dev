aa52e6818e6453967d1f8223f4a047d7e29adb5f
Tristan Gruener <tristan.gruener@gmail.com>
2017-02-26 23:37:50 -0800

    needed to order the list

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def this_week_widget
-    @classes = ClassPage.where(start_time: Time.zone.now.beginning_of_week..Time.zone.now.end_of_week)
+    @classes = ClassPage.where(start_time: Time.zone.now.beginning_of_week..Time.zone.now.end_of_week).order("start_time ASC").all
     # today = Date.today
     # seven_days_later = Time.new(Date.today.strftime('%Y'), Date.today.strftime('%m'), Date.today.strftime('%d').to_i + 7)
     # @classes = ClassPage.where(start_time: today..seven_days_later).order("start_time ASC").all
