e688bf26a7bcf357e661d1e45b9b40aa9e0f9f8e
Tristan Gruener <tristan.gruener@gmail.com>
2017-02-27 12:00:30 -0800

    catchup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def this_week_widget
-    #@classes = ClassPage.where(start_time: Time.zone.now.beginning_of_week..Time.zone.now.end_of_week).order("start_time ASC").all
-
     today = Date.today
     seven_days_later = today + 7
     @classes = ClassPage.where(start_time: today..seven_days_later).order("start_time ASC").all
-    # today = Date.today
-    # seven_days_later = Time.new(Date.today.strftime('%Y'), Date.today.strftime('%m'), Date.today.strftime('%d').to_i + 7)
-    # @classes = ClassPage.where(start_time: today..seven_days_later).order("start_time ASC").all
   end
 
   def announcement_widget
@@ -28,6 +23,5 @@ class ApplicationController < ActionController::Base
     this_week_widget
     announcement_widget
   end
-
 
 end
